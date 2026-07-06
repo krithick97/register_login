41bdf41e3d5302a12b48721340f748437d480ca2
krithick97 <krithick978@gmail.com>
2026-07-06 10:53:39 +0530

    with_sql

--- new_backend/app.js
+++ new_backend/app.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require ("helmet");
+const rateLimit = require ("express-rate-limit");
 
 const userRoutes =
 require("./routes/userroutes");
 
 const dashboardRoutes =
 require("./routes/dashboardRoutes");
-
+;
 const app = express();
 
+app.use(helmet());
 app.use(cors());
-
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 15 minutes
+  max: 2,                 // Maximum 100 requests
+  message: {
+    success: false,
+    message: "Too many requests. Please try again after 2 minutes."
+  }
+});
+
+app.use("/api",limiter);
 
 app.use(
     "/api/users",
